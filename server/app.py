fd4d3b301abfffb5500204caae897ec0c1c7158a
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-09-18 13:33:41 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -19,15 +19,69 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    if not animal:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>ID: {animal.id}</h1>
+        <h1>Name: {animal.name}</h1>
+        <h1>Species: {animal.species}</h1>
+        <h1>Zookeeper: {animal.zookeeper.name}</h1>
+        <h1>Enclosure: {animal.enclosure.environment}</h1>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    if not zookeeper:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    animals = '\n '.join([animal.name for animal in zookeeper.animals])
+
+    response_body = f'''
+        <h1>ID: {zookeeper.id}</h1>
+        <h1>Name: {zookeeper.name}</h1>
+        <h1>Birthday: {zookeeper.birthday}</h1>
+        <h1>Animal: {animals}</h1>
+        
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not enclosure:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    animals = ', '.join([animal.name for animal in enclosure.animals])
+
+    response_body = f'''
+        <h1>ID: {enclosure.id}</h1>
+        <h1>Environment: {enclosure.environment}</h1>
+        <h1>Open to Visitors: {enclosure.open_to_visitors}</h1>
+        <h1>Animal: {animals}</h1>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 
 if __name__ == '__main__':
